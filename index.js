19af7c91007a37fb6ea2195e429eb3cfc41c84a1
Fab <f.carrasco.marcos24@gmail.com>
2018-11-13 18:51:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ var fs = require('fs');
 var path = require('path');
 
 
-var SafariBrowser = function(baseBrowserDecorator) {
-  baseBrowserDecorator(this);
+var SafariBrowser = function (baseBrowserDecorator) {
+    baseBrowserDecorator(this);
 
-  this._start = function(url) {
-    var HTML_TPL = path.normalize(__dirname + '/safari.html');
-    var self = this;
+    this._start = function (url) {
+        var HTML_TPL = path.normalize(__dirname + '/safari.html');
+        var self = this;
 
-    fs.readFile(HTML_TPL, function(err, data) {
-      var content = data.toString().replace('%URL%', url);
-      var staticHtmlPath = self._tempDir + '/redirect.html';
+        fs.readFile(HTML_TPL, function (err, data) {
+            var content = data.toString().replace('%URL%', url);
 
-      fs.writeFile(staticHtmlPath, content, function(err) {
-        self._execCommand(self._getCommand(), [staticHtmlPath]);
-      });
-    });
-  };
+            // var staticHtmlPath = self._tempDir + '/redirect.html';
+            var staticHtmlPath = path.join(process.env.HOME, 'Library/Containers/com.apple.Safari/Data/redirect.html');
+
+
+            fs.writeFile(staticHtmlPath, content, function (err) {
+                if (err) {
+                    console.error(err);
+                    return false;
+                }
+                self._execCommand(self._getCommand(), [staticHtmlPath]);
+            });
+        });
+    };
 };
 
 SafariBrowser.prototype = {
